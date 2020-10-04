4b78e97f03fc61cf097a588ffbece2439619cb86
L0Z1K <qortmddbs731@korea.ac.kr>
2020-10-04 20:27:09 +0900

    add main.py

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # ResNet
+
+- ResNet 20 for CIFAR-10
+- Training time : 6m 17s
+- Top-5 Error : 82/10000 (0.82%)
+
+
+
+### References
+
+- [Deep Residual Learning for Image Recognition](https://arxiv.org/pdf/1512.03385.pdf)
+- [ResNets for CIFAR-10](https://towardsdatascience.com/resnets-for-cifar-10-e63e900524e0)
+
+
+
+### Results
+
+```bash
+[+] Train Start
+Epoch: 0, Cost: 1.756323
+Epoch: 1, Cost: 1.402689
+Epoch: 2, Cost: 1.178780
+Epoch: 3, Cost: 0.998107
+Epoch: 4, Cost: 0.870251
+Epoch: 5, Cost: 0.763952
+Epoch: 6, Cost: 0.692361
+Epoch: 7, Cost: 0.636958
+Epoch: 8, Cost: 0.592466
+Epoch: 9, Cost: 0.561714
+Epoch: 10, Cost: 0.535928
+Epoch: 11, Cost: 0.505024
+Epoch: 12, Cost: 0.482411
+Epoch: 13, Cost: 0.467861
+Epoch: 14, Cost: 0.457450
+Epoch: 15, Cost: 0.441384
+Epoch: 16, Cost: 0.423922
+Epoch: 17, Cost: 0.409067
+Epoch: 18, Cost: 0.395265
+Epoch: 19, Cost: 0.384215
+Epoch: 20, Cost: 0.384462
+Epoch: 21, Cost: 0.362018
+Epoch: 22, Cost: 0.359195
+Epoch: 23, Cost: 0.357122
+Epoch: 24, Cost: 0.346590
+Epoch: 25, Cost: 0.337465
+Epoch: 26, Cost: 0.330389
+Epoch: 27, Cost: 0.326831
+Epoch: 28, Cost: 0.317968
+Epoch: 29, Cost: 0.311805
+[+] Training time: 6m 17s
+[+] Test Start
+Top-5 Error: 82/10000 (0.82%)
+```
+
+---
---
+++ main.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import time
+
+transform = transforms.Compose([
+    transforms.Pad(4),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32),
+    transforms.ToTensor()
+])
+
+data_train = datasets.CIFAR10(root="CIFAR10/",
+                              train=True,
+                              download=True,
+                              transform=transform)
+
+data_test = datasets.CIFAR10(root="CIFAR10/",
+                             train=False,
+                             download=True,
+                             transform=transforms.ToTensor())
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, stride, 1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, 1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        if stride != 1: # When Downsampling,
+            self.downsample = nn.Sequential(
+                                nn.Conv2d(in_channels, out_channels, 3, 2, 1),
+                                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.downsample = None
+    
+    def forward(self, x):
+        out = self.bn1(self.conv1(x))
+        out = F.relu(out)
+        out = self.bn2(self.conv2(out))
+        if self.downsample is not None:
+            x = self.downsample(x)
+        out += x
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, n):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, 1, 1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = []
+        for _ in range(n):
+            self.layer1.append(ResidualBlock(16, 16, 1))
+        self.layer2 = [ResidualBlock(16, 32, 2)]
+        for _ in range(n-1):
+            self.layer2.append(ResidualBlock(32, 32, 1))
+        self.layer3 = [ResidualBlock(32, 64, 2)]
+        for _ in range(n-1):
+            self.layer3.append(ResidualBlock(64, 64, 1))
+        
+        self.layer1 = nn.Sequential(*self.layer1)
+        self.layer2 = nn.Sequential(*self.layer2)
+        self.layer3 = nn.Sequential(*self.layer3)
+
+        self.pool = nn.AvgPool2d(8)
+        self.linear = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.bn1(self.conv1(x))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
+
+data_loader = DataLoader(dataset=data_train,
+                         batch_size=256,
+                         shuffle=True,
+                         drop_last=True)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = ResNet(3).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1, weight_decay=0.0001, momentum=0.9)
+
+total_epoch = 30
+total_batch = len(data_loader)
+
+print("[+] Train Start")
+start = time.time()
+for epoch in range(total_epoch):
+    avg_cost = 0
+    for x, y in data_loader:
+        x = x.to(device)
+        y = y.to(device)
+        y_pred = model(x)
+        
+        cost = criterion(y_pred, y)
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        avg_cost += cost
+    avg_cost /= total_batch
+
+    print("Epoch: %d, Cost: %f" % (epoch, avg_cost))
+end = time.time()
+t = int(end - start)
+print("[+] Training time: %dm %ds" %(t//60, t%60))
+
+test_loader = DataLoader(dataset=data_test,
+                         batch_size=256,
+                         shuffle=False)
+
+print("[+] Test Start")
+def accuracy(output, target):
+    batch_size = target.size(0)
+    _, pred = output.topk(5, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    correct = correct[:5].view(-1).float().sum(0)
+    return correct
+
+total_test = len(data_test)
+acc = 0
+
+for x, y in test_loader:
+    x = x.to(device)
+    y = y.to(device)
+    y_pred = model(x)
+    acc += accuracy(y_pred, y)
+
+acc = total_test - acc
+print("Top-5 Error: %d/%d (%.2f%%)" % (acc, total_test, acc/total_test*100))
